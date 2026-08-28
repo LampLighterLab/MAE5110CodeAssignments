d1c9bf3777e5fcbbf29444a44e21af1da9fd09a8
Real-Human-1000 <1henryprendergast@gmail.com>
2026-08-28 14:13:35 -0400

    Refactored some comments to improve readability

--- assignment_0.py
+++ assignment_0.py
@@ -4,7 +4,7 @@ import timeit
 
 from models import pendulum as model
 from models import bouncing_ball as model_bb
-from integrators import explicit_euler as integrator
+from integrators import rk4 as integrator
 
 # Basic simulation of the pendulum
 
@@ -19,7 +19,7 @@ params = {
 initial_state = np.array([np.pi / 4, 0.0])
 
 timestep = 1e-3
-sim_time = 1.0
+sim_time = 5.0
 
 time_traj, state_traj = integrator.integrate(params, model, initial_state, timestep, sim_time)
 
@@ -38,89 +38,89 @@ time_traj, state_traj = integrator.integrate(params, model, initial_state, times
 # a stand-still.
 kinetic_energy, potential_energy = model.calculate_energy(state_traj, params)
 
-# Record the difference in energy observed, which we can correlate to numerical stability
-print(potential_energy[0] + kinetic_energy[0])
-print(potential_energy[-1] + kinetic_energy[-1])
-print(np.max(np.abs(potential_energy[0] + kinetic_energy[0] - potential_energy - kinetic_energy)))
+# Record the difference in energy observed (initial vs max diff), which we can correlate to numerical stability
+# print(potential_energy[0] + kinetic_energy[0])
+# print(potential_energy[-1] + kinetic_energy[-1])
+# print(np.max(np.abs(potential_energy[0] + kinetic_energy[0] - potential_energy - kinetic_energy)))
 
 # Plot the system energy over time
-# plt.figure()
-# plt.plot(time_traj, potential_energy, label="Potential energy")
-# plt.plot(time_traj, kinetic_energy, label="Kinetic energy")
-# plt.plot(time_traj, potential_energy + kinetic_energy, label="Total energy")
-# plt.xlabel("Time (s)")
-# plt.ylabel("Energy (J)")
-# plt.title("Pendulum energy")
-# plt.legend()
-# plt.tight_layout()
-# plt.show()
+plt.figure()
+plt.plot(time_traj, potential_energy, label="Potential energy")
+plt.plot(time_traj, kinetic_energy, label="Kinetic energy")
+plt.plot(time_traj, potential_energy + kinetic_energy, label="Total energy")
+plt.xlabel("Time (s)")
+plt.ylabel("Energy (J)")
+plt.title("Pendulum energy")
+plt.legend()
+plt.tight_layout()
+plt.show()
 
 # Forward-Euler timestep stability data from previous experiments
-# plt.figure()
-# stability_data = np.array([
-#     [1e-5, 0.0005905664312792491],
-#     [1e-4, 0.005900466086736],
-#     [1e-1, 5.072434753010766],
-#     [5e-2, 2.3230285667451853],
-#     [25e-3, 1.1463059377481968],
-#     [125e-4, 0.6712444854571763],
-#     [1e-3, 0.05851325093934279],
-#     [1e-2, 0.5452212444311582],
-#     [5e-3, 0.2831168543531102],
-#     [1e0, 130.42786394972003],
-#     [5e-1, 11.087846645768431],
-#     [2e-3, 0.11602547623693815],
-#     [9e-4, 0.05270637042693427],
-#     [5e-4, 0.02939008259857223],
-#     [3e-4, 0.017666905443722902],
-#     [2e-4, 0.011789502410201103]
-# ])
-# sort_idx = np.argsort(stability_data[:,0], axis=0)
-# stability_data = stability_data[sort_idx,:]
-# plt.loglog(stability_data[:,0], np.abs(stability_data[:,1]))
-# plt.xlabel("Timestep size (log)")
-# plt.ylabel("Maximum energy error")
-# plt.title("Energy Error vs Timestep Size for Forward Euler Pendulum Simulation")
-# plt.show()
+plt.figure()
+stability_data = np.array([
+    [1e-5, 0.0005905664312792491],  # This data was collected manually
+    [1e-4, 0.005900466086736],
+    [1e-1, 5.072434753010766],
+    [5e-2, 2.3230285667451853],
+    [25e-3, 1.1463059377481968],
+    [125e-4, 0.6712444854571763],
+    [1e-3, 0.05851325093934279],
+    [1e-2, 0.5452212444311582],
+    [5e-3, 0.2831168543531102],
+    [1e0, 130.42786394972003],
+    [5e-1, 11.087846645768431],
+    [2e-3, 0.11602547623693815],
+    [9e-4, 0.05270637042693427],
+    [5e-4, 0.02939008259857223],
+    [3e-4, 0.017666905443722902],
+    [2e-4, 0.011789502410201103]
+])
+sort_idx = np.argsort(stability_data[:,0], axis=0)
+stability_data = stability_data[sort_idx,:]
+plt.loglog(stability_data[:,0], np.abs(stability_data[:,1]))
+plt.xlabel("Timestep size (log)")
+plt.ylabel("Maximum energy error")
+plt.title("Energy Error vs Timestep Size for Forward Euler Pendulum Simulation")
+plt.show()
 # To keep energy within 1% of initial total energy, timestep must be smaller than 2e-4
 
 # Runge-Kutta timestep stability data from previous experiments
-# plt.figure()
-# stability_data = np.array([
-#     [1e-5, 3.7125857943465235e-13],
-#     [1e-4, 2.7977620220553945e-14],
-#     [1e-3, 1.255884285455977e-12],
-#     [1e-2, 1.0973600250707705e-08],
-#     [5e-2, 3.98738862614767e-05],
-#     [75e-3, 0.0002715263111844335],
-#     [1e-1, 0.0010764617586209169],
-#     [5e-1, 11.087846645768431],
-#     [1e0, 1.7652982276830052],
-#     [2e-1, 0.027450702331213273],
-#     [15e-2, 0.007343784236782502],
-#     [17e-2, 0.013252958709302765]
-# ])
-# sort_idx = np.argsort(stability_data[:,0], axis=0)
-# stability_data = stability_data[sort_idx,:]
-# plt.loglog(stability_data[:,0], np.abs(stability_data[:,1]))
-# plt.xlabel("Timestep size (log)")
-# plt.ylabel("Maximum energy error")
-# plt.title("Energy Error vs Timestep Size for Runge-Kutta Pendulum Simulation")
-# plt.show()
+plt.figure()
+stability_data = np.array([
+    [1e-5, 3.7125857943465235e-13],  # This data was collected manually
+    [1e-4, 2.7977620220553945e-14],
+    [1e-3, 1.255884285455977e-12],
+    [1e-2, 1.0973600250707705e-08],
+    [5e-2, 3.98738862614767e-05],
+    [75e-3, 0.0002715263111844335],
+    [1e-1, 0.0010764617586209169],
+    [5e-1, 11.087846645768431],
+    [1e0, 1.7652982276830052],
+    [2e-1, 0.027450702331213273],
+    [15e-2, 0.007343784236782502],
+    [17e-2, 0.013252958709302765]
+])
+sort_idx = np.argsort(stability_data[:,0], axis=0)
+stability_data = stability_data[sort_idx,:]
+plt.loglog(stability_data[:,0], np.abs(stability_data[:,1]))
+plt.xlabel("Timestep size (log)")
+plt.ylabel("Maximum energy error")
+plt.title("Energy Error vs Timestep Size for Runge-Kutta Pendulum Simulation")
+plt.show()
 # To keep energy within 1% of initial total energy, timestep must be smaller than 17e-2
 
 
 # Bouncing ball model
 params = model_bb.generate_params()
 
-# some set-up
+# Initial conditions for the model
 initial_state = np.array([0.0, 0.0, 1.0, 0.0])
 
 # We're keeping the timestep and sim length from the pendulum
 
 time_traj, state_traj = integrator.integrate(params, model_bb, initial_state, timestep, sim_time)
 
-# sanity check the energies: since there is no actuation, and no damping, total energy should stay
+# Sanity check the energies: since there is no actuation, and no damping, total energy should stay
 # constant. If we turn on the damping coefficient, it should slowly bleed out energy until it comes to
 # a stand-still.
 kinetic_energy, potential_energy = model_bb.calculate_energy(state_traj, params)
@@ -137,12 +137,12 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
-# Plot y position over time
+# Plot y position over kinetic energy
 plt.figure()
 plt.plot(time_traj, state_traj[2,:], label="Vertical pos")
 plt.ylabel("Vertical position")
 plt.xlabel("Time (s)")
-plt.title("Ball position over time")
+plt.title("Ball position over Time")
 plt.legend()
 plt.tight_layout()
 plt.show()
--- models/bouncing_ball.py
+++ models/bouncing_ball.py
@@ -9,29 +9,26 @@ def dynamics(t, state, params):
     # We have the issue that we don't know the timestep and can only set the derivatives of our state for one step
     # So we can't instantaneously change the position or velocity, since they only update by a tiny bit
 
-    print("State starting dynamics calc:")
-    print(state)
     gravity = params["gravity"]
     radius = params["radius"]
     mass = params["mass"]
-    coeff_restitution = params["coeff_restitution"]
+    elasticity = params["elasticity"]
+    damping = params["damping"]
 
     x = state[0]  # horizontal position
     xdot = state[1]  # horizontal velocity
     y = state[2]  # vertical position
     ydot = state[3]  # vertical velocity
 
-    ydotdot = -mass * gravity  # vertical acceleration
-    print(f"t={t:.02f}, y={y}, ydot={ydot}")
+    ydotdot = -gravity  # vertical acceleration
 
     if y - radius < 0:
         # The edge of the ball collides with the ground! Bounce it!
-        ydot = -ydot * coeff_restitution
-        print(f"y below ground at t={t}; setting ydot={ydot}")
-        # If we had direct control of position, we could also pop the ball above the ground
+        ydotdot += -((y - radius) * elasticity) / mass  # Simulated "springy" ground (or ball)
+        ydotdot += -ydot * damping  # Simulated damping losses from heat/friction in the ball
+        # If we had direct control of position, we could also pop position of the ball above the ground
 
     state_deriv = np.array([xdot, 0, ydot, ydotdot])
-    print(state_deriv)
     return state_deriv
 
 
@@ -41,13 +38,17 @@ def generate_params():
         "gravity": 9.81,  # gravity m/s^2)
         "radius": 0.1,  # ball radius (m)
         "mass": 0.5,  # mass of the ball (kg)
-        "coeff_restitution": 1.0,  # coefficient of restitution (ratio of final velocity to initial velocity)
+        "elasticity": 1000,  # spring constant for when the ball bounces
+        "damping": 0.5  # damping constant for when the ball bounces
     }
     return params
+
 
 def calculate_energy(state, params):
     gravity = params["gravity"]
     mass = params["mass"]
+    radius = params["radius"]
+    elasticity = params["elasticity"]
 
     xdot = state[1]  # horizontal velocity
     y = state[2]  # vertical position
@@ -55,8 +56,7 @@ def calculate_energy(state, params):
 
     kinetic_energy = 0.5 * mass * ydot**2 + 0.5 * mass * xdot**2
     potential_energy = y * mass * gravity
+    potential_energy += np.where(y - radius < 0, (0.5 * elasticity * (y - radius)**2), np.zeros_like(y))  # select only when the ball is contacting the ground
 
     return kinetic_energy, potential_energy
 
-
-# TODO: The ball doesn't bounce. Maybe model it as a spring that can compress to store energy before releasing it again?
