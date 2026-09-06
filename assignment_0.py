d896e1671bfa24b56077a41399f70157a4130215
ash33224 <ash332@cornell.edu>
2026-09-06 11:18:53 -0400

    Save assignment_0

--- assignment_0.py
+++ assignment_0.py
@@ -1,30 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
-<<<<<<< HEAD
 import timeit
 
 from models import pendulum as model
 from integrators import explicit_euler, rk4
 
-=======
-
-from models import pendulum as model
->>>>>>> upstream/main
 
 # Basic simulation of the pendulum
 
 params = {
-<<<<<<< HEAD
     "gravity": 9.81,  # gravity (m/s^2)
-=======
-    "gravity": 9.81,  # gravity m/s^2)
->>>>>>> upstream/main
     "length": 1,  # rod length (m)
     "mass": 0.2,  # point mass at end of rod (kg)
     "damping_coeff": 0.0,  # damping coefficient (kg*m^2/s)
 }
 
-<<<<<<< HEAD
 # some set-up
 initial_state = np.array([np.pi / 4, 0.0])
 
@@ -124,44 +114,10 @@ plt.plot(euler_time_traj, euler_potential_energy + euler_kinetic_energy, label="
 plt.xlabel("Time (s)")
 plt.ylabel("Energy (J)")
 plt.title(f"Explict Euler Pendulum Energy (dt = {euler_largest_stable_timestep:.5f})")
-=======
-
-# some set-up
-initial_state = np.array([np.pi / 4, 0.0])
-
-timestep = 1e-5
-sim_time = 5.0
-
-n_timesteps = int(sim_time / timestep) + 1
-time_traj = np.arange(n_timesteps) * timestep
-state_traj = np.zeros((2, n_timesteps))
-state_traj[:, 0] = initial_state
-
-# simulation loop
-for step, t in enumerate(time_traj[:-1]):
-    state_traj[:, step + 1] = state_traj[:, step] + timestep * model.dynamics(
-        t, state_traj[:, step], params
-    )
-
-# sanity check the energies: since there is no actuation, and no damping, total energy should stay
-# constant. If we turn on the damping coefficient, it should slowly bleed out energy until it comes to
-# a stand-still.
-
-potential_energy, kinetic_energy = model.calculate_energy(state_traj, params)
-
-plt.figure()
-plt.plot(time_traj, potential_energy, label="Potential energy")
-plt.plot(time_traj, kinetic_energy, label="Kinetic energy")
-plt.plot(time_traj, potential_energy + kinetic_energy, label="Total energy")
-plt.xlabel("Time (s)")
-plt.ylabel("Energy (J)")
-plt.title("Pendulum energy")
->>>>>>> upstream/main
 plt.legend()
 plt.tight_layout()
 plt.show()
 
-<<<<<<< HEAD
 # Phase portrait: plotting angular velocity vs angle
 plt.figure()
 plt.plot(euler_state_traj[0, :], euler_state_traj[1, :])
@@ -193,6 +149,3 @@ plt.title(f"Runge-Kutta Pendulum Phase Portrait (dt = {rk4_largest_stable_timest
 plt.axhline(0, color='k', linestyle='-', linewidth=1)  # Vertical line at x=0 to better see any deviations
 plt.tight_layout()
 plt.show()
-=======
-# TODO: make a phase portrait plot
->>>>>>> upstream/main
