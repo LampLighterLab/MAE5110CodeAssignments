d38bbc611c21cfe7aca190ff33cdb0802f2c7b26
Utku Melemetci <55263178+utkudotdev@users.noreply.github.com>
2026-09-08 15:08:51 -0400

    Refactor main

--- assignment_1.py
+++ assignment_1.py
@@ -1,51 +1,67 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-from models import rimless_wheel as model
 from integrators import rk4 as integrator
+from models import rimless_wheel as model
 
-params = model.generate_params()
 
-# theta, theta dot, base height
-initial_state = np.array([np.deg2rad(10.0), np.deg2rad(0.0), 0.0])
+def main():
+    params = model.generate_params()
 
-theta = initial_state[0]
-alpha, gamma = params["alpha"], params["gamma"]
-assert (gamma - alpha) < theta < (alpha + gamma)
+    # theta, theta dot, base height
+    initial_state = np.array([np.deg2rad(10.0), np.deg2rad(0.0), 0.0])
 
-timestep = 1e-4
-sim_time = 5.0
+    timestep = 1e-4
+    sim_time = 5.0
 
-n_timesteps = int(sim_time / timestep) + 1
-time_traj = np.arange(n_timesteps) * timestep
-state_traj = np.zeros((3, n_timesteps))
-state_traj[:, 0] = initial_state
+    time_traj, state_traj = simulate(initial_state, params, timestep, sim_time)
 
-# simulation loop
-for step, t in enumerate(time_traj[:-1]):
-    state_traj[:, step + 1] = model.discrete_dynamics(
-        state_traj[:, step], integrator, timestep, params
-    )
+    plot_traj(state_traj, time_traj, params)
 
-angle = state_traj[0]
-angular_momentum = model.calculate_angular_momentum(state_traj, params)
-kinetic_energy, potential_energy = model.calculate_energy(state_traj, params)
 
-fig, (ax1, ax2, ax3) = plt.subplots(nrows=3)
+def simulate(initial_state, params, timestep, sim_time):
+    theta = initial_state[0]
+    alpha, gamma = params["alpha"], params["gamma"]
+    assert (gamma - alpha) < theta < (alpha + gamma)
 
-ax1.plot(time_traj, angle, label="Angle")
-ax1.set_xlabel("Time (s)")
-ax1.set_ylabel("Angle (rad)")
+    n_timesteps = int(sim_time / timestep) + 1
+    time_traj = np.arange(n_timesteps) * timestep
+    state_traj = np.zeros((3, n_timesteps))
+    state_traj[:, 0] = initial_state
 
-ax2.plot(time_traj, angular_momentum, label="Angular momentum")
-ax2.set_xlabel("Time (s)")
-ax2.set_ylabel("Angular momentum")
+    # simulation loop
+    for step in range(n_timesteps - 1):
+        state_traj[:, step + 1] = model.discrete_dynamics(
+            state_traj[:, step], integrator, timestep, params
+        )
 
-ax3.plot(time_traj, kinetic_energy, label="Kinetic energy")
-ax3.plot(time_traj, potential_energy, label="Potential energy ")
-ax3.plot(time_traj, kinetic_energy + potential_energy, label="Total energy")
-ax3.set_xlabel("Time (s)")
-ax3.set_ylabel("Energy")
+    return time_traj, state_traj
 
-fig.legend()
-plt.show()
+
+def plot_traj(state_traj, time_traj, params):
+    angle = state_traj[0]
+    angular_momentum = model.calculate_angular_momentum(state_traj, params)
+    kinetic_energy, potential_energy = model.calculate_energy(state_traj, params)
+
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=3)
+
+    ax1.plot(time_traj, angle, label="Angle")
+    ax1.set_xlabel("Time (s)")
+    ax1.set_ylabel("Angle (rad)")
+
+    ax2.plot(time_traj, angular_momentum, label="Angular momentum")
+    ax2.set_xlabel("Time (s)")
+    ax2.set_ylabel("Angular momentum")
+
+    ax3.plot(time_traj, kinetic_energy, label="Kinetic energy")
+    ax3.plot(time_traj, potential_energy, label="Potential energy ")
+    ax3.plot(time_traj, kinetic_energy + potential_energy, label="Total energy")
+    ax3.set_xlabel("Time (s)")
+    ax3.set_ylabel("Energy")
+
+    fig.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
